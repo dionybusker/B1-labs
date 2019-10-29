8d7970cd32c8509fd6022f850fa114071a9dfffa
dionybusker <99057959@mydavinci.nl>
2019-10-29 11:39:48 +0100

    opdr 2
    work in progress

--- B1W8-functies/js/opdracht2.js
+++ B1W8-functies/js/opdracht2.js
@@ -1,1 +1,45 @@
 // opdracht 2
+
+var linebreak = "<br>";
+
+function addition(number1, number2) {
+    // var number1 = 10;
+    // var number2 = 12;
+    return number1 + number2;
+}
+
+function subtraction(number1, number2) {
+    // var number1 = 58;
+    // var number2 = 34;
+    return number1 - number2;
+}
+
+function multiplication(number1, number2) {
+    // var number1 = 6;
+    // var number2 = 7;
+    return number1 * number2;
+}
+
+function division(number1, number2) {
+    // var number1 = 144;
+    // var number2 = 12;
+    return number1 / number2;
+}
+
+function increment(number) {
+    // var number = 12;
+    return number = number + 1;
+}
+
+function decrement(number) {
+    // var number = 34;
+    return number = number - 1;
+}
+
+document.write(addition(10, 12) + linebreak);
+document.write(subtraction(58, 34) + linebreak);
+document.write(multiplication(6, 7) + linebreak);
+document.write(division(144, 12) + linebreak);
+document.write(increment(12) + linebreak);
+document.write(decrement(34) + linebreak);
+
