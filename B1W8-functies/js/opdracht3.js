c75ce041b946b175960a08ef540744e68f99bb3a
dionybusker <99057959@mydavinci.nl>
2019-10-29 14:57:21 +0100

    opdr 3
    work in progress, ik moet alleen de prompts zien te herhalen

--- B1W8-functies/js/opdracht3.js
+++ B1W8-functies/js/opdracht3.js
@@ -1,13 +1,33 @@
 // opdracht 3
 
-var naam = prompt("Wat is je naam?");
-var leeftijd = prompt("Wat is je leeftijd?");
+// var naam = prompt("Wat is je naam?");
+// var leeftijd = prompt("Wat is je leeftijd?");
 
-function name() {
-    // code
+function name(naam) {
+    var naam = prompt("Wat is je naam? Ben je klaar, vul dan 'stop' in.");
+    // if (naam != "stop") {
+    //     return naam;
+    // } else {
+    //     prompt("Wat is je naam?");
+    // }
+    return naam;
 }
 
-function age() {
-    // code
+function age(leeftijd) {
+    var leeftijd = prompt("Wat is je leeftijd? Ben je klaar, vul dan 'stop' in.");
+    // if (leeftijd != "stop") {
+    //     return leeftijd;
+    // } else {
+    //     prompt("Wat is je leeftijd?");
+    // }
+    return leeftijd;
 }
 
+// if (name(naam) != "stop") {
+//     name(naam);
+//     age(leeftijd);
+// } else {
+//     document.write("Hallo " + name() + ", je leeftijd is " + age() + " jaar.");
+// }
+
+document.write("Hallo " + name() + ", je leeftijd is " + age() + " jaar.");
