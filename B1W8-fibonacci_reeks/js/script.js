ec0d0528db0c68e289f8f202f21f1bd86ff9e372
dionybusker <99057959@mydavinci.nl>
2019-10-30 13:27:20 +0100

    lab fibonacci reeks
    gelukt en af, op twee manieren

--- B1W8-fibonacci_reeks/js/script.js
+++ B1W8-fibonacci_reeks/js/script.js
@@ -1,1 +1,39 @@
 // lab fibonacci reeks
+
+// var linebreak = "<br>"
+
+// function calc(a, b) {
+//     var c = a + b; // uitkomst
+//     for (var i = 1; i <= 20; i++) {
+//         document.write(a + " + " + b + " = " + c + linebreak);
+//         a = b;
+//         b = c;
+//         c = a + b;
+//     }
+//     return false;
+// }
+
+// calc(1, 1);
+
+// 1 + 1 = 2
+// 1 + 2 = 3
+// 2 + 3 = 5
+// 3 + 5 = 8
+
+
+var linebreak = "<br>"
+var i = 1;
+
+function calc(a, b) {
+    var c = a + b; // uitkomst
+    if (i <= 20) {
+        document.write(a + " + " + b + " = " + c + linebreak);
+        a = b;
+        b = c;
+        c = a + b;
+        i++
+        calc(a, b);
+    } 
+}
+
+calc(1, 1);
