760a9f0e3c373a3c942c19e133e6c3c5561c0bed
dionybusker <99057959@mydavinci.nl>
2019-10-30 11:56:53 +0100

    opdr 4
    af

--- B1W8-functies/js/opdracht4.js
+++ B1W8-functies/js/opdracht4.js
@@ -1,7 +1,20 @@
 // opdracht 4
 
-function calc(tafels) {
-    for (i = 1; i <= 10; i++) {
-        document.write(i+1 + "×" + )
+var linebreak = "<br>"
+
+function calc() {
+    var question = prompt("Hoeveel tafels wil je berekenen?"); // maximum aantal tafels
+    if (parseInt(question) >= 1 && parseInt(question) <= 10) {
+        for (i = 1; i <= question; i++) { // aantal verschillende tafels
+            for (var j = 1; j <= 10; j++){ // de hele tafel wordt helemaal uitgeschreven van 1 t/m 10
+            document.write(j + " × " + i + " = " + (i*j) + linebreak)
+            }
+            document.write(linebreak)
+        }
+    } else {
+        alert("Dit is geen correcte invoer, probeer opnieuw.");
+        calc();
     }
 }
+
+calc();
